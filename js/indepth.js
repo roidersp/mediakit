d8d899bffff9b25282ab347218ea44afdfdde9ea
Rodrigo Luis Santander <roidersp@gmail.com>
2015-04-27 14:02:07 -0500

    dsfsdfsd

--- js/indepth.js
+++ js/indepth.js
@@ -12,6 +12,53 @@ $(document).ready(function() {
 });
 
 
+window.onload = function() {
+	<!--  selecciono la clase javascript -->
+    var javascript = document.querySelector('.mexico');
+    new EasyPieChart(javascript, {
+			<!-- activo la animación y establezco su duración a un segundo -->
+			animate: ({ duration: 1000, enabled: true }),
+			<!-- aumento la longitud de las lineas de la gráfica -->
+			scaleLength:0,
+			<!-- aumento el tamaño de la gráfica -->
+			size:150,
+			barColor:'#1e2328',
+			trackColor: "#d4ced1",
+			lineWidth: 7,
+			rotate:0,
+			<!-- añado el número del porcentaje que se muestra en el span -->
+			onStep: function(from, to, percent) {
+				//this.el.children[0].innerHTML = Math.round(percent);
+			}
+    });
+	var html5 = document.querySelector('.usa');
+    new EasyPieChart(html5, {
+			animate: ({ duration: 2000, enabled: true }),
+			scaleLength:0,
+			size:150,
+			barColor:'#1e2328',
+			trackColor: "#d4ced1",
+			lineWidth: 6,
+			rotate:0,
+			onStep: function(from, to, percent) {
+				//this.el.children[0].innerHTML = Math.round(percent);
+			}
+    });
+	var css = document.querySelector('.mundo');
+    new EasyPieChart(css, {
+			animate: ({ duration: 3000, enabled: true }),
+			scaleLength:0,
+			size:150,
+			barColor:'#1e2328',
+			trackColor: "#d4ced1",
+			lineWidth: 5,
+			rotate:0,
+			onStep: function(from, to, percent) {
+				//this.el.children[0].innerHTML = Math.round(percent);
+			}
+    });
+	}
+
 /*$('#indepth_juanes').onepage_scroll({
 	 loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
    keyboard: true,                  // You can activate the keyboard controls
